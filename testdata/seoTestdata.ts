f3e8fbb2fd4b4650c15fc410fc6f66918148b52a
Pooja <pooja40jadeja@gmail.com>
2025-02-25 18:01:38 +0530

    added testdata file

---
+++ testdata/seoTestdata.ts
@@ -0,0 +1,20 @@
+export const expectedHreflangUrlsPlp =
+    [
+        'https://en.zalando.de/clothing/',
+        'https://www.zalando.co.uk/clothing/',
+        'https://www.zalando.ie/clothing/'
+    ];
+
+export const expectedHreflangUrlsBrands =
+    [
+        'https://en.zalando.de/nike/',
+        'https://www.zalando.co.uk/nike/',
+        'https://www.zalando.ie/nike/'
+    ];
+
+export const expectedHreflangCountries = ['en-de', 'en-gb', 'en-ie'];
+
+export const testUrls = {
+    brand: '/nike/',
+    plp: '/clothing/',
+}
